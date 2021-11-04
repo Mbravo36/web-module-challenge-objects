9f20c3b5089e11514ba46fb9a817d5f54fd2f70e
Your Name <mari@Maribels-MBP.hsd1.tx.comcast.net>
2021-11-04 16:14:24 -0500

    finished up to task 4

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+    return {name, price, category};
 }
+console.log('task 1a:', createMenuItem('tacos', 8, 'Lunch'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
@@ -28,7 +29,9 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
-
+console.log('task 1b:', createMenuItem('lasagna', 10, 'dinner'));
+console.log('task 1b:', createMenuItem('pizza', 12, 'lunch'));
+console.log('task 1b:', createMenuItem('pasta', 8, 'breakfast'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -47,9 +50,16 @@ const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  
+  discount: function (person){
+    if(person === "teacher" || person === "student"){
+      return this.price - (this.price * 0.25);
+    } else if (person === "public"){
+      return this.price - (this.price * 0.10);
+    }
+  }
 }
-
+console.log('task 2:', burger.discount("teacher"));
+console.log('task 2:', burger.discount("public"));
 
 
 ///////////////Reviews (MVP)///////////////////
@@ -69,7 +79,7 @@ Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
 
-
+console.log('task 3:', reviews[5].feedback);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -77,7 +87,7 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
-
+console.log(reviews[7].rating[this place is chill with really cool people, great for getting work done on weekdays]);
 
 
 
